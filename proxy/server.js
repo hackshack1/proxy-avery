815c4a909a14eb8af94077d7a2cedc5d044abd16
Avery <averylisneil@gmail.com>
2019-11-11 12:00:30 -0800

    Configured new relic. Proxy loads properly to localhost for recommendations microservice.

--- proxy/server.js
+++ proxy/server.js
@@ -1,17 +1,15 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 3004;
 
-const port = 3000;
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/:listingid', express.static(path.join(__dirname, 'public')));
 
-app.use('/air6n6/*/listing', express.static(path.join(__dirname, 'public')));
-
-app.get('/now', (req, res) => {
-  res.redirect('localhost:3010/now');
-});
 
 app.listen(port, () => {
   console.log(`App listening at ${port}`);
 });
 
-//54vh
+
